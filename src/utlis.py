774953fc0a70007bb96d005e490f83fdf512beb2
Udit Sharma <uditbouhare05@gmail.com>
2025-12-08 11:41:46 +0530

    huperparameter tuning

--- src/utlis.py
+++ src/utlis.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd 
 from src.exception import CustomException 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 
@@ -24,11 +25,27 @@ def save_object(file_path, obj):
     
 
     
-def evaluate_model(X_train,y_train,X_test,y_test,models): 
+def evaluate_model(X_train,y_train,X_test,y_test,models,param): 
     try: 
         report = {}
 
         for model_name, model in models.items(): 
+            param_grid = param.get(model_name,{})
+
+            if param_grid: 
+                gs = GridSearchCV(estimator=model,param_grid=param_grid,cv=3,n_jobs=-1)
+                gs.fit(X_train,y_train)
+
+                best_model = gs.best_estimator_
+            else: 
+                best_model=model
+                best_model.fit(X_train,y_train)
+
+            y_train_pred = best_model.predict(X_train)
+            y_test_pred = best_model.predict(X_test)
+
+            train_model_score = r2_score(y_train,y_train_pred)
+            train_model_score = r2_score(y_test,y_test_pred)
 
             model.fit(X_train,y_train)
 
